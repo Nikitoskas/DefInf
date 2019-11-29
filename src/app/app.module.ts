3a4bc03a48081df51bf0d7d84b905006fb08c364
Nikitoskas <1998nikitka@gmail.com>
2019-11-29 12:33:55 +0300

    created general form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+
 
 import { AppComponent } from './app.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { CryptComponent } from './crypt/crypt.component';
+import { DecryptComponent } from './decrypt/decrypt.component';
+import { AboutComponent } from './about/about.component';
+import { DevelopersComponent } from './developers/developers.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes = [
+  {path: ``, redirectTo: '/registration', pathMatch: 'full'},
+  {path: `registration`, component: RegistrationComponent},
+  {path: `crypt`, component: CryptComponent},
+  {path: `decrypt`, component: DecryptComponent},
+  {path: `about`, component: AboutComponent},
+  {path: `developers`, component: DevelopersComponent},
+  {path: `**`, component: PageNotFoundComponent}
+
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent,
+    CryptComponent,
+    DecryptComponent,
+    AboutComponent,
+    DevelopersComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
